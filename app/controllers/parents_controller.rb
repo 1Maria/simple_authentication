abf1bd4722abf2f5d1fc773436cdecc94ae196e9
Maria <mliberov@gmail.com>
2015-02-04 17:28:09 -0500

    Add rails files

---
+++ app/controllers/parents_controller.rb
@@ -0,0 +1,48 @@
+class ParentsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @parents = Parent.all
+  end
+
+  def show
+  end
+
+  def new
+    @parent = Parent.new
+  end
+
+  def edit
+  end
+
+  def create
+    @parent = Parent.new(parent_params)
+    if @parent.save
+      redirect_to @parent, notice: 'Parent was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @parent.update(parent_params)
+      redirect_to @parent, notice: 'Parent was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @parent.destroy
+    redirect to parents_url, notice: 'Parent was successfully destroyed.'
+  end
+
+  private
+  def set_parent
+    @parent = Parent.find(params[:id])
+  end
+
+  def parent_params
+    params.require(:parent).permit(:child_name, :parent_name, :email, :teacher_id, :created_at, :updated_at )
+  end
+end
